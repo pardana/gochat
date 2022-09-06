a74e6d8c61d48ddd51e33cec0414b50b375588df
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-06 16:44:52 +0700

    creating a chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/login"
+            render={(props) => {
+              return <h1>Login</h1>;
+            }}
+          />
+
+          <Route
+            path="/"
+            render={(props) => {
+              return <h1>Root</h1>;
+            }}
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
