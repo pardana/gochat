f44161bd5b22971be7492cd16cb625da3842de53
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-08 15:03:18 +0700

    Signup field required

--- src/components/partials/Signup.js
+++ src/components/partials/Signup.js
@@ -12,6 +12,7 @@ class Signup extends Component {
       passwordAgain: "",
       name: "",
       username: "",
+      error: "",
     };
   }
 
@@ -26,12 +27,29 @@ class Signup extends Component {
                 onSubmit={(e) => {
                   e.preventDefault();
                   if (this.props.socket) {
+                    let empty = 0;
+                    Object.keys(this.state).map((key) => {
+                      if (this.state[key] === "") {
+                        empty += 1;
+                      }
+                    });
+
+                    if (empty > 0) {
+                      return this.setState({ error: "All Fields Required" });
+                    } else {
+                      if (this.state.password !== this.state.passwordAgain) {
+                        return this.setState({ error: "Passwords Must Match" });
+                      }
+                    }
+
                     this.props.socket.send(
                       JSON.stringify({
                         type: "SIGNUP",
                         data: {
                           email: this.state.email,
                           password: this.state.password,
+                          name: this.state.name,
+                          username: this.state.username,
                         },
                       })
                     );
@@ -41,6 +59,9 @@ class Signup extends Component {
                 <p>
                   Already have an account? <Link to="/login">Login</Link>
                 </p>
+                {this.state.error ? (
+                  <p className="text-danger">{this.state.error}</p>
+                ) : null}
                 <div className="row">
                   <div className="col-md-6">
                     <div className="form-group">
